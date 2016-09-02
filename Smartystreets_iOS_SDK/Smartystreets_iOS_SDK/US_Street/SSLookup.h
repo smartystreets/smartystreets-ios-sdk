3f42c51127e398a21927b60c2561e966dc4638ca
Oshion Niemela <oshion@smartystreets.com>
2016-09-02 13:06:51 -0600

    Created the SSLookup class for the US_Street API.

---
+++ Smartystreets_iOS_SDK/Smartystreets_iOS_SDK/US_Street/SSLookup.h
@@ -0,0 +1,21 @@
+#import <Foundation/Foundation.h>
+#import "SSCandidate.h"
+
+@interface SSLookup : NSObject
+
+@property (nonatomic) NSString *inputId;
+@property (nonatomic) NSString *street;
+@property (nonatomic) NSString *street2;
+@property (nonatomic) NSString *secondary;
+@property (nonatomic) NSString *city;
+@property (nonatomic) NSString *state;
+@property (nonatomic) NSString *zipCode;
+@property (nonatomic) NSString *lastline;
+@property (nonatomic) NSString *addressee;
+@property (nonatomic) NSString *urbanization;
+
+- (instancetype)initWithFreeformAddress:(NSString*)freeformAddress;
+- (void)addToResult:(SSCandidate*)newCandidate;
+- (void)setMaxCandidates:(int)maxCandidates error:(NSError**)error;
+
+@end
---
+++ Smartystreets_iOS_SDK/Smartystreets_iOS_SDK/US_Street/SSLookup.m
@@ -0,0 +1,38 @@
+#import "SSLookup.h"
+
+@interface SSLookup()
+
+@property (nonatomic) NSMutableArray<SSCandidate*> *result;
+@property (nonatomic) int maxCandidates;
+
+@end
+
+@implementation SSLookup
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _maxCandidates = 1;
+        _result = [[NSMutableArray<SSCandidate*> alloc] init];
+    }
+    return self;
+}
+
+- (instancetype)initWithFreeformAddress:(NSString*)freeformAddress {
+    if (self = [[super self] init])
+        _street = freeformAddress;
+    return self;
+}
+
+- (void)addToResult:(SSCandidate*)newCandidate {
+    [self.result addObject:newCandidate];
+}
+
+- (void)setMaxCandidates:(int)maxCandidates error:(NSError**)error {
+    if (maxCandidates > 0)
+        _maxCandidates = maxCandidates;
+    
+    NSDictionary *details = @{NSLocalizedDescriptionKey: @"Max candidates must be a positive integer."};
+    *error = [NSError errorWithDomain:NSCocoaErrorDomain code:nil userInfo:details]; //TODO: what is the error code?
+}
+
+@end
