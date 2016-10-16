7883ffb0de2743c4d2513a085193c33bd48aecf6
Oshion Niemela <oshion@smartystreets.com>
2016-10-16 01:09:16 -0600

    Progress commit for SSHttpSender

--- Smartystreets_iOS_SDK/Smartystreets_iOS_SDK/SSHttpSender.m
+++ Smartystreets_iOS_SDK/Smartystreets_iOS_SDK/SSHttpSender.m
@@ -24,8 +24,45 @@
     return self;
 }
 
-- (SSResponse*)sendRequest:(SSRequest*)request withError:(NSError**)error {
-    return nil; //TODO: implement
+- (SSResponse*)sendRequest:(SSRequest*)request error:(NSError**)error {
+    NSMutableURLRequest *httpRequest = [self buildHttpRequest:request];
+    [self copyHeaders:request httpRequest:httpRequest];
+    
+    return [self buildResponse:httpRequest];
+}
+
+- (NSMutableURLRequest*)buildHttpRequest:(SSRequest*)request {
+    NSURL *url = [NSURL URLWithString:[request getUrl]];
+    
+    NSMutableURLRequest *httpRequest = [NSMutableURLRequest requestWithURL:url];;
+    if ([request.method isEqualToString:@"GET"]) {
+        [httpRequest setHTTPMethod:@"GET"];
+    }
+    else {
+        [httpRequest setHTTPMethod:@"POST"];
+        [httpRequest setHTTPBody:request.payload];
+    }
+    return httpRequest;
+}
+
+- (void)copyHeaders:(SSRequest*)request httpRequest:(NSMutableURLRequest*)httpRequest {
+    for (NSString *key in [request.headers allKeys])
+        [httpRequest addValue:request.headers[key] forHTTPHeaderField:key];
+}
+
+- (SSResponse*)buildResponse:(NSMutableURLRequest*)httpRequest {
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    
+    __block SSResponse *myResponse;
+    [[session dataTaskWithRequest:httpRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error == nil) {
+            int statusCode = (int)[(NSHTTPURLResponse *) response statusCode];
+            NSData *payload = data;
+            myResponse = [[SSResponse alloc] initWithStatusCode:statusCode payload:payload];
+        }
+    }] resume];
+    
+    return myResponse;
 }
 
 @end
