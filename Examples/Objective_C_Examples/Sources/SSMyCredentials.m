9e7368869060aa810edd80e04fd12a198b472bf7
Oshion Niemela <oshion@smartystreets.com>
2016-12-09 11:28:58 -0700

    Updated example code.

--- Examples/Objective_C_Examples/Sources/SSMyCredentials.m
+++ Examples/Objective_C_Examples/Sources/SSMyCredentials.m
@@ -1,10 +1,10 @@
 #import "SSMyCredentials.h"
 
-NSString *const kSSSmartyWebsiteKey = @"7356597903161930";
-NSString *const kSSHost = @"smartystreets.com";
+NSString *const kSSSmartyWebsiteKey = @"KEY";
+NSString *const kSSHost = @"HOST";
 
-NSString *const kSSAuthId = @"50e075ce-76d4-6746-0cde-fe806bb949b5";
-NSString *const kSSAuthToken = @"GaJtRq9bgz8Al6QjwzAN";
+NSString *const kSSAuthId = @"ID";
+NSString *const kSSAuthToken = @"TOKEN";
 
 @implementation SSMyCredentials
 
--- Examples/Swift_Examples/Sources/MyCredentials.swift
+++ Examples/Swift_Examples/Sources/MyCredentials.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 struct MyCredentials {
-    static let SmartyWebsiteKey = "7356597903161930";
-    static let Host = "smartystreets.com";
+    static let SmartyWebsiteKey = "KEY";
+    static let Host = "HOST";
     
-    static let AuthId = "50e075ce-76d4-6746-0cde-fe806bb949b5";
-    static let AuthToken = "GaJtRq9bgz8Al6QjwzAN";
+    static let AuthId = "ID";
+    static let AuthToken = "TOKEN";
 }
