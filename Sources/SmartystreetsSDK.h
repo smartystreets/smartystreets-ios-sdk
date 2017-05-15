205dd69cfc87b86a233427f55df508a2d5c93eaf
Oshion Niemela <oshion@smartystreets.com>
2017-05-15 17:12:26 -0600

    Added support for macOS projects

--- Sources/SmartystreetsSDK.h
+++ Sources/SmartystreetsSDK.h
@@ -1,11 +1,3 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for SmartystreetsSDK.
-FOUNDATION_EXPORT double SmartystreetsSDKVersionNumber;
-
-//! Project version string for SmartystreetsSDK.
-FOUNDATION_EXPORT const unsigned char SmartystreetsSDKVersionString[];
-
 // In this header, you should import all the public headers of your framework using statements like #import <SmartystreetsSDK/PublicHeader.h>
 #import "SSRequest.h"
 #import <SmartystreetsSDK/SSInternationalStreetClient.h>
