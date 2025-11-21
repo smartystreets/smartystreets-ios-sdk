68b646b5db0fa1208b0d49fcea32989a28d56c2f
Jacob McKenzie <159198001+JacobMcKenzieSmarty@users.noreply.github.com>
2025-11-21 16:46:36 -0700

    Fully remove enrichment property financial. (#50)

--- samples/Sources/swiftExamples/USEnrichmentExample.swift
+++ samples/Sources/swiftExamples/USEnrichmentExample.swift
@@ -54,9 +54,6 @@ class USEnrichmentExample{
             if lookupType.lowercased() == "principal" {
                 let results = client.sendPropertyPrincipalLookup(inputLookup: lookup, error: &error)
                 return try self.outputPrincipalResults(results: [results?[0]])
-            } else if lookupType.lowercased() == "financial" {
-                let results = client.sendPropertyFinancialLookup(inputLookup: lookup, error: &error)
-                return try self.outputFinancialResults(results: [results?[0]])
             } else if lookupType.lowercased() == "geo-reference" {
                 let results = client.sendGeoReferenceLookup(inputLookup: lookup, error: &error)
                 return try self.outputGeoReferenceResults(results: [results?[0]])
@@ -75,9 +72,6 @@ class USEnrichmentExample{
         } else if lookupType.lowercased() == "principal" {
             let results = client.sendPropertyPrincipalLookup(smartyKey: smartyKey, error: &error)
             return try self.outputPrincipalResults(results: [results?[0]])
-        } else if lookupType.lowercased() == "financial" {
-            let results = client.sendPropertyFinancialLookup(smartyKey: smartyKey, error: &error)
-            return try self.outputFinancialResults(results: [results?[0]])
         } else if lookupType.lowercased() == "geo-reference" {
             let results = client.sendGeoReferenceLookup(smartyKey: smartyKey, error: &error)
             return try self.outputGeoReferenceResults(results: [results?[0]])
@@ -94,33 +88,7 @@ class USEnrichmentExample{
         
         return "lookupType unknown"
     }
-    
-    func outputFinancialResults(results: [FinancialResult?]) throws -> String {
-        if let error = error {
-            let output = """
-            Domain: \(error.domain)
-            Error Code: \(error.code)
-            Description:\n\(error.userInfo[NSLocalizedDescriptionKey] as! NSString)
-            """
-            NSLog(output)
-            return output
-        }
-        
-        var output = "Results: \n"
-        
-        for result in results {
-            let encoder = JSONEncoder()
-            let jsonData = try encoder.encode(result)
-            let jsonString = String(data: jsonData, encoding: .utf8)
-            output.append(jsonString ?? "[]")
-            output.append("\n******************************\n")
-        }
-        
-        output.append("\n******************************\n")
 
-        return output
-    }
-    
     func outputPrincipalResults(results: [PrincipalResult?]) throws -> String {
         if let error = error {
             let output = """
